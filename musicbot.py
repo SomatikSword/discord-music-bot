9c69a79afe6b5a35ae3f4b5b9e0eac3b213594b7
Anastasiya Surzhikova <surzhikova94@inbox.ru>
2026-04-21 03:16:56 +0300

    add flask web server

--- musicbot.py
+++ musicbot.py
@@ -2,13 +2,27 @@ import discord
 import os
 import feedparser
 import random
+import threading
+from flask import Flask
 from dotenv import load_dotenv
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 
 load_dotenv()
 
 DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
-CHANNEL_ID = int(os.getenv("1495926078001385573"))
+CHANNEL_ID = int(os.getenv("CHANNEL_ID"))
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Bot is alive"
+
+
+def run_web():
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
+
 
 intents = discord.Intents.default()
 client = discord.Client(intents=intents)
@@ -26,6 +40,10 @@ def get_youtube_video(query):
 
 async def send_ost():
     channel = client.get_channel(CHANNEL_ID)
+
+    if channel is None:
+        print("Канал не найден")
+        return
 
     queries = [
         "Star Wars The Old Republic OST",
@@ -55,5 +73,7 @@ async def on_ready():
     print(f"Бот запущен как {client.user}")
     scheduler.start()
 
+
+threading.Thread(target=run_web).start()
 
 client.run(DISCORD_TOKEN)
